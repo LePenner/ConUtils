dce66cc73b771511a3dc8ce80a607be25698c931
Bismarck <erikblunk42@gmail.com>
2025-04-04 00:57:02 +0200

    implement error class for json read error

--- src/conutils/Spinner.py
+++ src/conutils/Spinner.py
@@ -1,5 +1,7 @@
+import json
+
 class Spinner():
-    def __init__(self, spn_type):
+    def __init__(self, spn_type = str):
         if spn_type not in Spinner.spinners:
             raise Exception('Invalid spinner type')
 
@@ -9,12 +11,11 @@ class Spinner():
         self.seq_list = Spinner._generate_sequence_list(self)
 
     
-
     spinners = {"default" : {"seq" : '|/-\\',
                              "div" : 1}}
  
     @classmethod
-    def reg_spn(cls, name, seq, div):
+    def reg_spn(cls, name = str, seq = str, div = int):
         if name in cls.spinners:
             raise Exception('This type of spinner already exists, please choose another name')
         elif len(seq) % div != 0:
@@ -22,9 +23,32 @@ class Spinner():
         
         cls.spinners[name] = {"seq" : seq, 
                               "div" : div }
-        
+    
+    """json format as follows: {<spinner_name> : {"seq" : <str>, "div" : <int>}}"""
+    @classmethod
+    def load_json(cls, file_ = str, replace = False):
+        with open(file_) as json_file:
+            loaded_file = json.load(json_file)
 
-    def change_spn_to(self, spn_type):
+        # format check
+        for spinner in loaded_file.items():
+            # get dict as tuple out of tuple
+            # e = (key, value)
+            for e in spinner[1].items():
+                match e[0]: 
+                    case "seq":
+                        if type(e[1]) != str:
+                            raise JsonFromatError('seq', spinner[0])
+                    case "div":
+                        if type(e[1]) != int:
+                            raise JsonFromatError('div', spinner[0])
+                    case _:
+                        raise JsonFromatError('keys', spinner[0])
+        
+        # if format is correct
+        # -- code goes here --
+   
+    def change_spn_to(self, spn_type = str):
         if spn_type not in Spinner.spinners:
             raise Exception('Invalid spinner type')
         
@@ -36,5 +60,23 @@ class Spinner():
 
     def _generate_sequence_list(self):
         return [self.seq[i:i+self.div] for i in range(0, len(self.seq), self.div)]
-    def load_json(self):
-        pass
+
+            
+class JsonFromatError(Exception):
+    def __init__(self, error_type, element, message = 'Invalid json format'):
+        if error_type == 'keys':
+            self.message = message + f'\n  key error: on {element}\n'
+        if error_type == 'seq':
+            self.message = message + f'\n  value error for "seq" expected str on: {element}\n'
+        if error_type == 'div':
+            self.message = message + f'\n  value error for "div" expected int on: {element}\n'
+        else:  
+            self.message == "Unkown Error"
+
+        super().__init__(self.message)
+
+
+        
+spn = Spinner("default")
+
+Spinner.load_json("test.json")
