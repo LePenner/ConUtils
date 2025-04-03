4c9990a9eca54884eac60e239df8e51b8be8b26d
Bismarck <erikblunk42@gmail.com>
2025-04-04 01:13:36 +0200

    Spinner cleanup

--- src/conutils/Spinner.py
+++ src/conutils/Spinner.py
@@ -10,20 +10,20 @@ class Spinner():
         self.div = Spinner.spinners[spn_type]["div"]
         self.seq_list = Spinner._generate_sequence_list(self)
 
-    
+
     spinners = {"default" : {"seq" : '|/-\\',
                              "div" : 1}}
- 
+
     @classmethod
     def reg_spn(cls, name = str, seq = str, div = int):
         if name in cls.spinners:
             raise Exception('This type of spinner already exists, please choose another name')
         elif len(seq) % div != 0:
             raise Exception('Sequence needs to be divisible by divider')
-        
+
         cls.spinners[name] = {"seq" : seq, 
                               "div" : div }
-    
+
     """json format as follows: {<spinner_name> : {"seq" : <str>, "div" : <int>}}"""
     @classmethod
     def load_json(cls, file_ = str, replace = False):
@@ -44,14 +44,14 @@ class Spinner():
                             raise JsonFromatError('div', spinner[0])
                     case _:
                         raise JsonFromatError('keys', spinner[0])
-        
+
         # if format is correct
         # -- code goes here --
-   
+
     def change_spn_to(self, spn_type = str):
         if spn_type not in Spinner.spinners:
             raise Exception('Invalid spinner type')
-        
+
         self.spn_type = spn_type
         self.seq = Spinner.spinners[spn_type]["seq"]
         self.div = Spinner.spinners[spn_type]["div"]
@@ -67,16 +67,11 @@ class JsonFromatError(Exception):
         messages = { 'seq' : 'value error for "seq" expected str',
                      'div' : 'value error for "div" expected int',
                      'key' : 'key error'}
-        
+
         if key in messages:
             self.message = messages[key]
         else:
             self.message = 'unknown error'
 
         super().__init__('Invalid json format\n  ' + self.message + f' on: {element}\n')
-
-
         
-spn = Spinner("default")
-
-Spinner.load_json("test.json")
