00f36c06fdcbbe6e526801571578f5f661c03ed0
Bismarck <erikblunk42@gmail.com>
2025-04-07 14:55:22 +0200

    stable state Entity

--- src/conutils/entity/entity.py
+++ src/conutils/entity/entity.py
@@ -12,28 +12,31 @@ class Entity():
             parent.add_child(self)
             self.parent = parent
 
+    # ----- setter: position -----
+
     def set_x(self, x):
         if self.parent:
             if self.parent.get_width() < self._width + x:
                 raise StructureError('edge conflict')
         self._x = x
 
-    def get_x(self):
-        return self._x
-
     def set_y(self, y):
-
         if self.parent:
             if self.parent.get_height() < self._height + self._y:
                 raise StructureError('edge conflict')
         self._y = y
 
-    def get_y(self):
-        return self._y
-
     def move(self, x, y):
         self.set_x(x)
         self.set_y(y)
+
+    # ----- getter: position -----
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
 
     def get_x_abs(self):
         if self.parent:
@@ -47,6 +50,26 @@ class Entity():
         else:
             return self._y
 
+    # ----- setter: dimension -----
+
+    def set_width(self, width):
+        if self.parent:
+            if self.parent.get_width() < self._x + width:
+                raise StructureError('edge conflict')
+        self.width = width
+
+    def set_heigth(self, height):
+        if self.parent:
+            if self.parent.get_width() < self._y + height:
+                raise StructureError('edge conflict')
+        self.width = height
+
+    def dimensions(self, width, height):
+        self.set_width(width)
+        self.set_heigth(height)
+
+    # ----- getter: dimension -----
+
     def get_width(self):
         return self._width
 
@@ -58,7 +81,7 @@ class StructureError(Exception):
     def __init__(self, key):
         messages = {'parent double': "specified child already has parent associated, try 'replace=True'",
                     'child not found': "not a child of given container",
-                    'edge conflict': "specified movement conflicts with size of container"
+                    'edge conflict': "specified displacement conflicts with size of container"
                     }
 
         if key in messages:
